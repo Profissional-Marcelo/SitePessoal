3706d4c58e0caeda1ca5603ebd6dd8ec28599e60
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2023-12-27 00:08:58 -0300

    adicionando os pdf, terminando a quarta parte e aplicando a responsividade

--- App/View/paginaInicial.php
+++ App/View/paginaInicial.php
@@ -15,8 +15,7 @@
 
 <body>
 
-      <section class="primeiraParte">
-
+    <section class="primeiraParte">
         <div>
             <p class="quemSou">OI 👋 EU SOU O MARCELO</p>
             <div class="container">
@@ -29,22 +28,24 @@
                     <a href="https://github.com/Profissional-Marcelo" target="_blank">
                         <i class="devicon-github-original "></i></a>
 
-                    <a href="src/downloads/Curriculo.pdf" type="application/pdf" target="_blank" rel="next">
+
+                    <a href="../../Assets/pdf/curriculo.pdf" type="application/pdf" rel="next" target="_blank">
+
                         <img src="../../Assets/img/curriculo.png" alt="curriculo"></a>
 
                 </div>
             </div>
             <h1 class="titulo2"><strong translate="no">DEVELOPER</strong></h1>
 
-                <div class="paragrafo">
-                    <p class="story">TENHO 20 ANOS, SOU DESENVOLVEDOR FRONTEND COM 5 MESES DE EXPERIENCIA TRABALHANDO COMO FREELANCER</p>
-                </div>
-            
+            <div class="paragrafo">
+                <p class="story">TENHO 20 ANOS, SOU DESENVOLVEDOR FRONTEND COM 5 MESES DE EXPERIENCIA TRABALHANDO COMO FREELANCER</p>
+            </div>
+
         </div>
 
-    </section> 
+    </section>
 
-     <section class="segundaParte">
+    <section class="segundaParte">
         <div class="espacamento">
             <div class="projetos">
                 <a href="https://memsolucoestecnologicas.com.br/App/View/login.php" target="_blank">
@@ -54,7 +55,7 @@
                 </a>
             </div>
             <div class="projetos">
-                <a href="http://www.marcelohenrique.kinghost.net/App/View/paginaInicial.php">
+                <a href="http://www.marcelohenrique.kinghost.net/App/View/paginaInicial.php" target="_blank">
                     <img class="tablet2" src="../../Assets/img/tablet2.png" alt="projeto">
                     <p>HOST MENTOR</p>
                     <h2>Site auxiliar de trabalho com gamequiz</h2>
@@ -63,10 +64,10 @@
         </div>
         <div class="espacamento">
             <div class="projetos">
-                <a href="">
-                    <img src="../../Assets/img/tablet.png" alt="projeto">
+                <a href="https://estudos.marcelorei.com.br" target="_blank">
+                    <img class="tablet3" src="../../Assets/img/tablet3.png" alt="projeto">
                     <p>ESTUDOS</p>
-                    <h2>Site para consultar códigos de projetos que repliquei anteriormente.</h2>
+                    <h2>Site para consultar códigos de projetos que repliquei anteriormente</h2>
                 </a>
             </div>
 
@@ -78,112 +79,132 @@
                 </a>
             </div>
         </div>
-    </section> 
+    </section>
 
-    <section class="quartaParte">
+    <section class="quartaParte" id="certificados">
         <h2>Certificados<span class="colorPonto">.</span></h2>
         <img src="../../Assets/img/fadergs.png" alt="">
         <div class="certificados">
-            <div class="linha1">
-                <div class="bordaCertificado">
-                    <div class="certificado">
-                        <h2>Tecnologo</h2>
-                        <p>Análise e desenvolvimento de sistemas</p>
-                        <h2>FADERGS</h2>
-                        <h2>2022 - 2024</h2>
+            <div class="linhas">
+                <a href="#" type="application/pdf" rel="next" target="_blank">
+                    <div class="bordaCertificado">
+                        <div class="certificado">
+                            <h2>Tecnologo</h2>
+                            <p>Análise e desenvolvimento de sistemas</p>
+                            <h2>FADERGS</h2>
+                            <h2>2022 - 2024</h2>
+                        </div>
                     </div>
-                </div>
-                <div class="bordaCertificado">
-                    <div class="certificado">
-                        <h2>Tecnologo</h2>
-                        <p>Análise e desenvolvimento de sistemas</p>
-                        <h2>FADERGS</h2>
-                        <h2>2022 - 2024</h2>
+                </a>
+                <a href="#" type="application/pdf" rel="next" target="_blank">
+                    <div class="bordaCertificado">
+                        <div class="certificado">
+                            <h2>Tecnologo</h2>
+                            <p>Análise e desenvolvimento de sistemas</p>
+                            <h2>FADERGS</h2>
+                            <h2>2022 - 2024</h2>
+                        </div>
                     </div>
-                </div>
-                <div class="bordaCertificado">
-                    <div class="certificado">
-                        <h2>Tecnologo</h2>
-                        <p>Análise e desenvolvimento de sistemas</p>
-                        <h2>FADERGS</h2>
-                        <h2>2022 - 2024</h2>
+                </a>
+                <a href="../../Assets/pdf/CertificadoProgramação.pdf" type="application/pdf" rel="next" target="_blank">
+                    <div class="bordaCertificado">
+                        <div class="certificado">
+                            <h2>Curso</h2>
+                            <p>Programação de soluções computacionais</p>
+                            <h2>FADERGS</h2>
+                            <h2>JUN/2023</h2>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
-            <div class="linha2">
-            <div class="bordaCertificado">
-                    <div class="certificado">
-                        <h2>Tecnologo</h2>
-                        <p>Análise e desenvolvimento de sistemas</p>
-                        <h2>FADERGS</h2>
-                        <h2>2022 - 2024</h2>
+            <div class="linhas">
+                <a href="../../Assets/pdf/CertificadoModelagem.pdf" type="application/pdf" rel="next" target="_blank">
+                    <div class="bordaCertificado">
+                        <div class="certificado">
+                            <h2>Curso</h2>
+                            <p>Modelagem de software</p>
+                            <h2>FADERGS</h2>
+                            <h2>JUN/2023</h2>
+                        </div>
                     </div>
-                </div>
-                <div class="bordaCertificado">
-                    <div class="certificado">
-                        <h2>Tecnologo</h2>
-                        <p>Análise e desenvolvimento de sistemas</p>
-                        <h2>FADERGS</h2>
-                        <h2>2022 - 2024</h2>
+                </a>
+                <a href="../../Assets/pdf/CertificadoSegurança.pdf" type="application/pdf" rel="next" target="_blank">
+                    <div class="bordaCertificado">
+                        <div class="certificado">
+                            <h2>Curso</h2>
+                            <p>Sistemas computacionais e segurança</p>
+                            <h2>FADERGS</h2>
+                            <h2>DEZ/2022</h2>
+                        </div>
                     </div>
-                </div>
-                <div class="bordaCertificado">
-                    <div class="certificado">
-                        <h2>Tecnologo</h2>
-                        <p>Análise e desenvolvimento de sistemas</p>
-                        <h2>FADERGS</h2>
-                        <h2>2022 - 2024</h2>
+                </a>
+                <a href="../../Assets/pdf/CertificadoAmbientes.pdf" type="application/pdf" rel="next" target="_blank">
+                    <div class="bordaCertificado">
+                        <div class="certificado">
+                            <h2>Curso</h2>
+                            <p>Ambientes computacionais e conectividade</p>
+                            <h2>FADERGS</h2>
+                            <h2>DEZ/2022</h2>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
-            <div class="linha3">
-            <div class="bordaCertificado">
-                    <div class="certificado">
-                        <h2>Tecnologo</h2>
-                        <p>Análise e desenvolvimento de sistemas</p>
-                        <h2>FADERGS</h2>
-                        <h2>2022 - 2024</h2>
+            <div class="linhas">
+                <a href="../../Assets/pdf/CertificadoProductOwner.pdf" type="application/pdf" rel="next" target="_blank">
+                    <div class="bordaCertificado">
+                        <div class="certificado">
+                            <h2>Evento</h2>
+                            <p>SATIGN 2023: O Papel do Product Owner</p>
+                            <h2>FADERGS</h2>
+                            <h2>24/10/2023</h2>
+                        </div>
                     </div>
-                </div>
-                <div class="bordaCertificado">
-                    <div class="certificado">
-                        <h2>Tecnologo</h2>
-                        <p>Análise e desenvolvimento de sistemas</p>
-                        <h2>FADERGS</h2>
-                        <h2>2022 - 2024</h2>
+                </a>
+                <a href="../../Assets/pdf/certificadoComplexo.pdf" type="application/pdf" rel="next" target="_blank">
+                    <div class="bordaCertificado">
+                        <div class="certificado">
+                            <h2>Evento</h2>
+                            <p>SEE 2023 - O Complexo de Parobé</p>
+                            <h2>FADERGS</h2>
+                            <h2>23/05/2023</h2>
+                        </div>
                     </div>
-                </div>
-                <div class="bordaCertificado">
-                    <div class="certificado">
-                        <h2>Tecnologo</h2>
-                        <p>Análise e desenvolvimento de sistemas</p>
-                        <h2>FADERGS</h2>
-                        <h2>2022 - 2024</h2>
+                </a>
+                <a href="../../Assets/pdf/CertificadoEmpreenderFGTAS.pdf" type="application/pdf" rel="next" target="_blank">
+                    <div class="bordaCertificado">
+                        <div class="certificado">
+                            <h2>Evento</h2>
+                            <p>SEE 2023 - O Empreender FGTAS</p>
+                            <h2>FADERGS</h2>
+                            <h2>25/05/2023</h2>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
         </div>
     </section>
 
 
-     <section class="terceiraParte">
+    <section class="terceiraParte">
         <h2>Fale comigo</h2>
         <form action="faleComigo.php" method="post" id="faleComigo">
 
 
-            <div class="border1"><input type="text" nome="email" minlength="8" maxlength="100" id="iemail" size="24" required placeholder="E-mail" autocomplete="email"><div class="icon">
-                <span class="material-symbols-outlined">
+            <div class="border1"><input type="text" nome="email" minlength="8" maxlength="100" id="iemail" size="24" required placeholder="E-mail" autocomplete="email">
+                <div class="icon">
+                    <span class="material-symbols-outlined">
                         mail
                     </span>
-            </div></div>
+                </div>
+            </div>
 
             <div class="border2"><textarea rows="10" maxlength="500" required placeholder="Deixe sua ideia ou sugestão" id="icomment"></textarea></div>
 
             <button type="" id="submitButton"><img src="../../Assets/img/botaoEnviar.png" alt="botao de envio"></button>
         </form>
-    </section>   
-     <script src="../../Assets/js/jquery.js"></script>
-    <script src="../../Assets/js/script.js"></script> 
+    </section>
+    <script src="../../Assets/js/jquery.js"></script>
+    <script src="../../Assets/js/script.js"></script>
 </body>
 
 </html>
