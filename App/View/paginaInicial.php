185a8f90a710b0cbb525c25c725406a87d80f907
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-01-10 13:15:02 -0300

    certificado joomla

--- App/View/paginaInicial.php
+++ App/View/paginaInicial.php
@@ -297,7 +297,7 @@
                             <h2>Curso</h2>
                             <p>CMS[Módulo2] - WordPress</p>
                             <h2>KingHost</h2>
-                            <h2>2023</h2>
+                            <h2>2024</h2>
                         </div>
                     </div>
                 </a>
@@ -365,13 +365,13 @@
                         </div>
                     </div>
                 </a>
-                <a href="../../Assets/pdf/certificadoComplexo.pdf" type="application/pdf" rel="next" target="_blank">
+                <a href="../../Assets/pdf/KingHost/Modulo3(CMS)-Joomla.pdf" type="application/pdf" rel="next" target="_blank">
                     <div class="bordaCertificado">
                         <div class="certificado">
                             <h2>Evento</h2>
-                            <p>SEE 2023 - O Complexo de Parobé</p>
-                            <h2>FADERGS</h2>
-                            <h2>23/05/2023</h2>
+                            <p>CMS[Módulo3] - Joomla</p>
+                            <h2>KingHost</h2>
+                            <h2>2024</h2>
                         </div>
                     </div>
                 </a>
