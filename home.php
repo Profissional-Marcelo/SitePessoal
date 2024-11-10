7951dd4999ccb84c4b05466f2fa94e447f15cd90
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-11-10 13:55:06 -0300

    .

---
+++ home.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../../Assets/css/home.css">
+    <title>P2K WIKI</title>
+  
+</head>
+
+<body>
+    
+<?php require_once("header.php"); ?>
+
+<section class="home">
+    <section class="atalhos">
+        <a href="#" class="item">
+            <div class="item">
+                <div class="corpo">
+               <img width="100" height="100" src="https://img.icons8.com/ios/100/FFFFFF/monitor--v1.png" alt="monitor--v1"/>
+                </div>
+                <div class="descricao">
+                    <p>Arquitetura PDV</p>
+                </div>
+            </div>
+        </a>
+        <a href="#" class="item">
+            <div class="item">
+                <div class="corpo"><img class="bluebird "width="100" height="100" src="https://img.icons8.com/ios-filled/100/FFFFFF/phonelink-setup.png" alt="phonelink-setup"/></div>
+                <div class="descricao">
+                    <p>Configurações VM</p>
+                </div>
+            </div>
+        </a>
+        <a href="#" class="item">
+            <div class="item">
+                <div class="corpo"><img class="relogio" width="100" height="100" src="https://img.icons8.com/ios/100/FFFFFF/time--v1.png" alt="time--v1"/></div>
+                <div class="descricao">
+                    <p>Reprocessamento de notas</p>
+                </div>
+            </div>
+        </a>
+        </section>
+        <section class="atalhos atalhos2">
+
+        
+        <a href="#" class="item">
+            <div class="item">
+                <div class="corpo"><img width="100" height="100" src="https://img.icons8.com/ios/100/FFFFFF/available-updates.png" alt="available-updates"/></div>
+                <div class="descricao">
+                    <p>Processar transação no RESA</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="#" class="item">
+            <div class="item">
+                <div class="corpo">
+                <img width="100" height="100" src="https://img.icons8.com/ios-filled/100/FFFFFF/bank-card-back-side.png" alt="bank-card-back-side"/>
+                </div>
+                <div class="descricao">
+                    <p>Inserir forma de pagamento</p>
+                </div>
+            </div>
+        </a>
+
+        <a href="#" class="item">
+            <div class="item">
+                <div class="corpo"><img width="100" height="100" src="https://img.icons8.com/ios-filled/100/FFFFFF/high-priority.png" alt="high-priority"/></div>
+                <div class="descricao">
+                    <p>A DEFINIR</p>
+                </div>
+            </div>
+        </a>
+
+        </section>
+</section>
+
+</body>
