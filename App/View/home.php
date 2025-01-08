a9c2e811d782b90c51d2eab54fc36d6c0141e4a5
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2025-01-08 00:39:07 -0300

    refazendo o portfolio, concluindo a primeira parte

---
+++ App/View/home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Marcelo Henrique Cornelis Rei</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0" />
+    <link rel="stylesheet" href="../../Assets/css/home.css">
+    <link rel="stylesheet" href="../../Assets/css/homeMQ.css">
+    <link rel="shortcut icon" href="../../Assets/img/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+
+    <section class="primeira-parte">
+        <div class="w100">
+            <p class="quem-sou">Oi 👋 eu sou o Marcelo</p>
+                <div class="titulo-1">
+                    <h1>web</h1>
+                    <div class="icones">
+
+                        <a href="https://www.linkedin.com/in/marcelo-rei-8246a226a/" target="_blank">
+                            <i class="devicon-linkedin-plain"></i></a>
+
+                        <a href="https://github.com/Profissional-Marcelo" target="_blank">
+                            <i class="devicon-github-original "></i></a>
+
+
+                        <a href="../../Assets/pdf/curriculo.pdf" type="application/pdf" rel="next" target="_blank">
+
+                            <img src="../../Assets/img/curriculo.png" alt="curriculo"></a>
+
+                    </div>
+                </div>
+                <div class="titulo-2">
+                    <h1>developer</h1>
+                    <div class="paragrafo">
+                        <p class="historia">tenho 21 anos e possuo vasta experiência e interesse pelas áreas de desenvolvimento web.</p>
+                    </div>
+                </div>
+
+        </div>
+    </section>
+
+
+    <script src="../../Assets/js/jquery.js"></script>
+    <script src="../../Assets/js/home.js"></script>
+</body>
+
+</html>
